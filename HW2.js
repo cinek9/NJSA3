82aad4361bd019f097afac382c50bb03f255c527
Marcin Kowalski <98392991+cinek9@users.noreply.github.com>
2022-01-27 11:39:08 +0100

    Create HW2.js

---
+++ HW2.js
@@ -0,0 +1,13 @@
+<script>
+
+function elementsInRage(min, max) {
+    var range = [];
+    for (let i = min; i <= max; i++) {
+        range.push(i);
+    }
+    return range;
+}
+
+elementsInRage(1, 5)
+
+</script>
