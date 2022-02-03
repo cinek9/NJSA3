36d6613a87ffe738c34a2a0a4b954d8c66170364
Marcin Kowalski <98392991+cinek9@users.noreply.github.com>
2022-02-03 19:14:10 +0100

    Update HW1.js

--- HW1.js
+++ HW1.js
@@ -1,3 +1,5 @@
+zad.1
+
 <script>
 
 function isSubsting() {
@@ -11,3 +13,26 @@ alert(result);
 }
 
 </script>
+
+//zad.2
+
+    const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const array2 = [5, 8];
+
+    let max = array2[1], min = array2[0];
+    for (let i = 0; i < array2.length; i++) {
+  
+        // If we get any element in array greater
+        // than max, max takes value of that
+        // larger number
+        if (array1[i] > max) { max = array2[i]; }
+  
+        // If we get any element in array smaller
+        // than min, min takes value of that
+        // smaller number
+        if (array1[i] < min) { min = array2[i]; }
+
+        for(let i = min; i <= max; i++){
+            console.log(i);
+    }
+}
