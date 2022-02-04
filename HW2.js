4b2d305d8b17de9eb51f5f77c022e4865e9ff4ca
Marcin Kowalski <98392991+cinek9@users.noreply.github.com>
2022-02-04 11:52:28 +0100

    Update HW2.js

--- HW2.js
+++ HW2.js
@@ -52,6 +52,29 @@ const arr = [6, 2, 10, 4, 2, -1, 3, 5]
 solution3(arr) // [6, 10, 4, 5] w tym przypadku średnią jest 3.875
 */
 
+function getAverage(arr) {
+    let sum = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+  
+  function solution3(arr) {
+    let avg = getAverage(arr);
+    let numbers = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > avg) {
+        numbers.push(arr[i]);
+      }
+    }
+  
+    return numbers;
+  }
+  const arr = [6, 2, 10, 4, 2, -1, 3, 5]
+  solution3(arr);
 
 /* zad. 4. Funkcja solution4 przyjmuje text. Zwraca najdłuższe zdanie
 const text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. '
