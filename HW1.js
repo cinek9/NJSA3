3f556b4e99303dee010b435c366b79c0cab55ee8
Marcin Kowalski <98392991+cinek9@users.noreply.github.com>
2022-02-04 14:58:29 +0100

    Update HW1.js

--- HW1.js
+++ HW1.js
@@ -50,23 +50,16 @@ elementsInRage([1,2,3,4,5,6,7,8,9,10,11] , 5, 8) // [5, 6, 7, 8] elementsInRage(
 elementsInRage([1,2,3,4,5,6,7,8,9,10,11] , 100, 120) // [] Proszę wykorzystać do każdego zadania pętli, jeżeli znacie lepsze (estetycznie) rozwiązanie zróbcie 
 to jako drugą wersję na przykład: elementsInRange2 */
 
-    const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    const array2 = [5, 8];
-
-    let max = array2[1], min = array2[0];
-    for (let i = 0; i < array2.length; i++) {
-  
-        // If we get any element in array greater
-        // than max, max takes value of that
-        // larger number
-        if (array1[i] > max) { max = array2[i]; }
-  
-        // If we get any element in array smaller
-        // than min, min takes value of that
-        // smaller number
-        if (array1[i] < min) { min = array2[i]; }
-
-        for(let i = min; i <= max; i++){
-            console.log(i);            // ?? zwraca poprawny wynik, ale nie przeszukuje tablicy array1 - nie wiem, jak to zrobić i obudować w funkcję.
+    function elementsInRage(min, max) {
+    
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    
+    for (let i = min; i <= max; i++) {
+        console.log(i);
+        array.push(i);
     }
+    return array;
 }
+
+elementsInRage(5, 8)
+/* => [5, 6, 7, 8] */
